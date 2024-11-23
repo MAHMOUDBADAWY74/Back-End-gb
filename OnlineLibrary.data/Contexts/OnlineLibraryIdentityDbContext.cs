6e509c950802b90cdfc5d2e779af27f1fcf36294
MAHMOUDBADAWY74 <105337014+MAHMOUDBADAWY74@users.noreply.github.com>
2024-11-23 07:36:49 +0200

    first commit

---
+++ OnlineLibrary.data/Contexts/OnlineLibraryIdentityDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineLibrary.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Data.Contexts
+{
+   public class OnlineLibraryIdentityDbContext : IdentityDbContext<ApplicationUser>
+
+    {
+
+
+        public OnlineLibraryIdentityDbContext(DbContextOptions<OnlineLibraryIdentityDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ OnlineLibrary.data/Entities/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Data.Entities
+{
+    public class ApplicationUser :IdentityUser
+
+    {
+        public String? firstName {  get; set; }
+
+        public String? LastName { get; set; }
+        public Address Address { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public String? Gender { get; set; }
+
+    }
+}
